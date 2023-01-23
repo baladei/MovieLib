a4769a1d6cb509624b73eb5733be35e2fbf0f84d
Fernando Baladei <f.baladei@gmail.com>
2023-01-23 06:39:20 -0300

    configurating API request and creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'
+
+import { Navbar } from './components/Navbar';
 
 import './App.css'
 
@@ -6,14 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <nav id="navbar">
-        <h2>
-          <Link to='/'>MoviesLib</Link>
-        </h2>
-          <Link to='/movie/1'>Movie</Link>
-          <Link to='/search'>Search</Link>
-      </nav>
-      <h2>Movie Lib</h2>
+      <Navbar />      
       <Outlet />
     </div>
   )
