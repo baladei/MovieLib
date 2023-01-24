784c296958111287c5dd452827c484ecd6cee22e
Fernando Baladei <f.baladei@gmail.com>
2023-01-24 06:39:57 -0300

    upgrading API request and configure styles css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
+import "./Navbar.css"
 
 export const Navbar = () => {
   return (
@@ -8,7 +9,7 @@ export const Navbar = () => {
           <Link to='/'><BiCameraMovie />MoviesLib</Link>
         </h2>
         <form>
-            <input type="text" placeholder="|Busque um filme" />
+            <input type="text" placeholder="Busque um filme" />
             <button type="submit">
               <BiSearchAlt2 />
             </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { MovieCard } from "../components/MovieCard";
+import "./MoviesGrid.css"
 
 
 const moviesURL = import.meta.env.VITE_API;
@@ -30,7 +32,7 @@ const Home = () => {
       <h2 className="title">Melhores filmes:</h2>
       <div className="movies-container">
         {topMovies.length === 0 && <p>Carregando...</p>}
-        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
   )
